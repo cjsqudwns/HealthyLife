c25325b3fbfdecee5a3e2eea08425ef08a73d159
cjsqudwns <59379460+cjsqudwns@users.noreply.github.com>
2023-05-30 15:52:44 +0900

    doing:회원가입 activity add

---
+++ app/src/main/java/com/example/healthylife/RegisterActivity.kt
@@ -0,0 +1,14 @@
+package com.example.healthylife
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.healthylife.databinding.ActivityRegisterBinding
+
+class RegisterActivity : AppCompatActivity() {
+    lateinit var binding:ActivityRegisterBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
